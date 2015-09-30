83dc3a12ac363cec4e005f1783ea5fb853918894
Jean-Philippe Guillermo <gjp@ciblo.net>
2015-09-30 11:37:47 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# wordpress-starter
-Quick Wordpress theme starter including Underscores theme + SASS
+
+starter_v1
+===
+
+Developers, make your own wordpress theme from scratch with starter_v1, starter_v1 is based on underscores http://underscores.me/ Enjoy :)
+
+Getting Started
+---------------
+
+This theme including boostrap.
+
+All the style should be in imports folder, use compass in your theme folder for tracking every changes.
+
+Now you're ready to go! The next step is easy to say, but harder to do: make an awesome WordPress theme. :)
+
+Good luck!
---
+++ template-parts/content-page.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The template used for displaying page content in page.php
+ *
+ * @package starter_v1
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'starter_v1' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php edit_post_link( esc_html__( 'Edit', 'starter_v1' ), '<span class="edit-link">', '</span>' ); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
+
